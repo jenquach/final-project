66ff5dfa63bfcef6e3c43e1ee525273c2ce355a1
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2022-01-20 21:48:31 +0100

    Got all cart endpoints to work

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const cartRouter = require('./routers/cart')
 const indexRouter = require('./routers/index')//require('./db/mongoose')
+const cookieParser = require("cookie-parser");
 require('./mongo')
+
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -13,6 +16,7 @@ const app = express()
 // // Add middlewares to enable cors and json body parsing
 // app.use(cors())
 app.use(express.json())
+app.use(cookieParser());
 app.use(indexRouter)
 app.use(cartRouter)
 
