bcb9bdd36ea4d0bba4934a4d2f7aa08ebe0b6086
Kaliine <80845418+Kaliine@users.noreply.github.com>
2022-01-24 20:49:20 +0100

    feat: footer, added mui styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 
 import Footer from "./components/Footer"
 
+
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,11 +3,15 @@ import Box from "@mui/material/Box"
 import Grid from "@mui/material/Grid"
 import Link from "@mui/material/Link"
 import styled from "styled-components"
-import { Container, Typography } from "@mui/material"
+import { Typography } from "@mui/material"
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 
 const FooterWrapper = styled.section`
 background: #A9CDCE;
+font-weight: bold;
 `
 
 const Footer = () => {
@@ -17,35 +21,46 @@ const Footer = () => {
         sx={{
         display: 'flex',
         flexDirection: { xs: 'column', sm: 'row' },
-        height: '100px',
-        alignItems: 'center',
+        height: '120px',
+        paddingTop: '40px',
+        paddingBottom: '20px',
         justifyContent: 'space-evenly',
+        textTransform: 'uppercase',
+        fontWeight: 'normal',
+        padding: '10px, 0',
         }}>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={8}>
+            <Grid item xs={12} sm={10}>
               <Box>
                 <Link href="/" color="inherit" underline="none">About us</Link>
               </Box>
             </Grid>
           </Grid>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={8}>
+            <Grid item xs={12} sm={10}>
               <Box>
                 <Link href="/" color="inherit" underline="none">Terms and conditions</Link>
               </Box>
             </Grid>
           </Grid>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={8}>
+            <Grid item xs={12} sm={10}>
               <Box>
                 <Link href="/" color="inherit" underline="none">Customer Service</Link>
               </Box>
             </Grid>
           </Grid>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={8}>
+            <Grid item xs={12} sm={10}>
               <Box>
                 <Link href="/" color="inherit" underline="none">Contact</Link>
+                <Box sx={{
+                      padding: '10px',
+                    }}>
+                  <MailOutlineIcon />
+                  <FacebookIcon />
+                  <InstagramIcon />
+                </Box>
               </Box>
             </Grid>
           </Grid>
@@ -53,13 +68,14 @@ const Footer = () => {
       <Box
         sx={{
         backgroundColor: '#CFE8E0',
-        height: '60px',
+        height: '80px',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        padding: '10px, 0',
         }}>
-          <Typography>
-            This is a ficional webshop ...
+          <Typography variant="subtitle1">
+            This is a fictional webshop made by students at Technigo Bootcamp
           </Typography>
       </Box>
     </FooterWrapper>
