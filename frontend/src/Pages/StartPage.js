e7d8aedfb796b4f1f0f1b08b1bb582f872c53d35
Kaliine <80845418+Kaliine@users.noreply.github.com>
2022-01-26 21:27:21 +0100

    feat: frontend start page, added styling

--- frontend/src/Pages/StartPage.js
+++ frontend/src/Pages/StartPage.js
@@ -1,50 +1,33 @@
 import React from "react"
-/* import Box from "@mui/material/Box"
-import Grid from "@mui/material/Grid"
-import Card from "@mui/material/Card" */
 import { makeStyles } from "@mui/styles"
-
 import CategoryCard from "../components/CategoryCard"
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minHeight: '90vh',
-    backgroundColor: '#F9F9F9',
-    color: 'white',
-    display: 'flex',
-    flexDirection: 'column',
-    
-   /*  [theme.breakpoints.down("md")]:{
-      flexDirection: 'row',
-    }, */
+    backgroundColor: "#F9F9F9",
+    color: "white",
+    display: "flex",
+    flexDirection: "column",
   },
   heroImage: {
     position: "relative",
     backgroundImage: `url(${require("../assets/toddler_choosing_clothes.jpg")})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
     backgroundPosition: "center center",
-    height: '80vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    height: "120vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  /* featuredCategories: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: "wrap",
-    justifyContent: 'space-evenly',
-    paddingTop: '40px',
-    paddingBottom: '60px',
-  } */
   featuredCategories: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))',
-    gridGap: '2%',
-    paddingTop: '40px',
-    paddingBottom: '60px',
-    justifyContent: 'space-evenly',
-    justifyItems: 'center',
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+    gridGap: "2%",
+    paddingTop: "40px",
+    paddingBottom: "60px",
+    justifyContent: "space-evenly",
+    justifyItems: "center",
   }
 }))
 
@@ -63,7 +46,5 @@ const Startpage = () => {
     </div>
   )
 }
-
-
 
 export default Startpage
