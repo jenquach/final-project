bd3e5bc272cf971bb94a745efbc46b4636d1fe4c
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2022-01-26 20:57:41 +0100

    Added MUIgrids to display products nicely

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,25 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import ProductBox from './ProductBox';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+import FixedContainer from './FixedContainer';
 
 const ProductList = () => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
 
-    useEffect( () => {
-        fetch('http://localhost:8080/products/') //option is needed otherwise is going to be getMetod
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                setProducts(data);
-            });
-    }, []);
+  useEffect(() => {
+    fetch('http://localhost:8080/products/') //option is needed otherwise is going to be getMetod
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      });
+  }, []);
 
-    return (
-        <>
-            {products.map((product) => (<ProductBox key={product.productId} product={product}></ProductBox>))}
-        </>
-    );
-};
 
+  return (
+    <>
+    <FixedContainer>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 2, md: 4 }}>
+          {products.map((product) => (
+            <Grid item xs={1} sm={1} md={1} key={product.productId}>
+              <ProductBox key={product.productId} product={product}></ProductBox>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+      </FixedContainer>
+    </>
+  );
+}
 
 export default ProductList;
