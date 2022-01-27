19cdeac0536ca8b237a3504ae277d087301276b2
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2022-01-27 22:29:45 +0100

    Add to cart cartId in localStorage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Footer from "./components/Footer"
 import ResponsiveAppBar from "./components/Navbar"
 import ProductList from './components/ProductList';
 
+import {v4 as uuidv4 } from 'uuid'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DummyStartPage from './components/DummyStartPage';
 
 
+
 function App() {
+
+  useEffect(() => {
+    if (localStorage.getItem("cartId"))
+    return
+    localStorage.setItem("cartId", uuidv4());
+    }, []);
+
   return (
     <div className="App">
       <BrowserRouter>
--- frontend/src/components/ProductBox.js
+++ frontend/src/components/ProductBox.js
@@ -2,11 +2,25 @@ import React from "react";
 import Button from '@mui/material/Button';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
+import { API_URL } from '../utils/urls'
+
 
 const ProductBox = ({ product }) => {
 const handleAddToCartClick = (event) => {
+console.log(event)
 
-    
+const options = {
+    method: 'POST',
+    headers: {
+        cartId: localStorage.getItem("cartId"),
+    },
+};
+
+fetch ( API_URL('cart/add-item?itemId=' + product.productId),options) //option is needed otherwise is going to be getMetod
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
 }
 
     return (<>
@@ -18,7 +32,7 @@ const handleAddToCartClick = (event) => {
         variant="outlined" 
         color="primary" 
         endIcon={<AddShoppingCartIcon />}
-        onClick={handleAddToCartClick}
+        onClick={()=> handleAddToCartClick(product)}
         >
         Buy
       </Button>
