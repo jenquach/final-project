f09100b25b5640d224e34c54d5957b32d90838f4
Jenny Quach <quach.jen@gmail.com>
2022-01-28 22:55:10 +0100

    set up redux store and reducer for user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Provider } from "react-redux"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
 
-import Footer from "./components/Footer"
+import Signup from './components/Signup'
+import SignInSide from './components/SignInSide'
 import ResponsiveAppBar from "./components/Navbar"
+import Footer from "./components/Footer"
 
+import user from "./reducers/user"
+import profile from "./reducers/profile"
 
-function App() {
+const reducer = combineReducers({
+	user: user.reducer,
+	profile: profile.reducer,
+})
 
+const store = configureStore({ reducer })
 
+export const App = () => {
   return (
-    <div className="App">
-    <ResponsiveAppBar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    <Footer />
-    </div>
-  );
+    <Provider store={store}> 
+      <BrowserRouter>
+      <ResponsiveAppBar />
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            {/* <Route path="/profile" element={<Profile />} /> */}
+          </Routes>
+      </BrowserRouter>
+      <Footer />
+    </Provider>
+  )
 }
-
-export default App;
---
+++ frontend/src/reducers/user.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const user = createSlice({
+	name: 'user',
+	initialState: {
+		userId: null,
+		email: null,
+		accessToken: null,
+		error: null,
+	},
+	reducers: {
+		setUserId: (store, action) => {
+			store.userId = action.payload
+		},
+		setEmail: (store, action) => {
+			store.email = action.payload
+		},
+		setAccessToken: (store, action) => {
+			store.accessToken = action.payload;
+		},
+    deleteAccessToken: (store, action) => {
+      store.accessToken = null
+    },
+    deleteUserId: (store, action) => {
+      store.userId = null
+    },
+		setError: (store, action) => {
+			store.error = action.payload
+		},
+	},
+})
+
+export default user
