74293321791d3dbe9d43fbaa66da3573a02f9582
Kaliine <80845418+Kaliine@users.noreply.github.com>
2022-02-01 23:27:24 +0100

    feat/frontend-product-details, fetching details

--- frontend/src/Pages/ProductDetails.js
+++ frontend/src/Pages/ProductDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { /* useParams */ /* useNavigate */ } from "react-router-dom"
+import { useParams /* useNavigate */ } from "react-router-dom"
 import Box from "@mui/material/Box"
 import Card from "@mui/material/Card"
 import CardContent from "@mui/material/CardContent"
@@ -8,67 +8,67 @@ import Typography from "@mui/material/Typography"
 import styled from "styled-components"
 
 const ProductDetailsWrapper = styled.div`
-    margin: 100px 10px 50px 10px;
+  margin: 100px 10px 50px 10px;
 `
 
 const ProductDetails = () => {
+  const { productId } = useParams()
   const [product, setProduct] = useState({})
-  const PRODUCT_URL = `https://final-project--api.herokuapp.com/product/1`
-  
-  /* const { productId } = useParams()
-  const PRODUCT_URL = `https://final-project--api.herokuapp.com/products/${productId}` */
+  /* const PRODUCT_URL = `https://final-project--api.herokuapp.com/product/1` */
+
+  const PRODUCT_URL = `https://final-project—api.herokuapp.com/product/${productId}`
+
 
   /* const navigate = useNavigate() */
 
-  
-   useEffect(() => {
-     fetch ( PRODUCT_URL )
+  useEffect(() => {
+    fetch (PRODUCT_URL)
       .then((res) => res.json())
       .then((json) => {
         setProduct(json)
       })
-  }, [PRODUCT_URL])
+}, [PRODUCT_URL])
 
-  /* const BackToAllProducts = () => {
-		navigate('/products')
-  } */
+/* const BackToAllProducts = () => {
+    navigate('/products')
+  } */
 
   return(
-      <ProductDetailsWrapper>
-      <Card sx={{ display: "flex", minHeight: "300px"}} >
-        <CardMedia
+  <ProductDetailsWrapper>
+    <Card sx={{ display: "flex", minHeight: "300px"}} >
+      <CardMedia
         component="img"
         sx={{ width: 151, padding: "20px", margin: "0 40px" }}
         image={ product.img1 }
         alt={ product.productName }
-        />
-        <Box key={product.productId} sx={{ display: "flex", flexDirection: "column" }}>
-          <CardContent sx={{ flex: "1 0 auto" }}>
-            <Typography component="div" variant="h4" color="#A9CDCE">
-              { product.productName }
-            </Typography>
-            <Typography variant="h6" color="#A9CDCE" component="div" marginBottom={2}>
-              £{ product.price }.00
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary" component="div">
-              { product.desc }
-            </Typography>
-            {/* <Typography sx={{ fontWeight: 'bold' }}>
-              Condition
-            </Typography> */}
-            <Typography variant="subtitle1" color="text.secondary" component="div">
-              { product.condition }
-            </Typography>
-          </CardContent>
-            <button>
-              BUY
-            </button>
-            <button className="back-btn" /* onClick={BackToAllProducts} */>
-              Back To All Products
-            </button>
+      />
+      <Box key={product.productId} sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+        <Typography component="div" variant="h4" color="#A9CDCE">
+          { product.productName }
+        </Typography>
+        <Typography variant="h6" color="#A9CDCE" component="div" marginBottom={2}>
+          £{ product.price }.00
+        </Typography>
+        <Typography variant="subtitle1" color="text.secondary" component="div">
+          { product.desc }
+        </Typography>
+        {/* <Typography sx={{ fontWeight: 'bold' }}>
+              Condition
+            </Typography> */}
+        <Typography variant="subtitle1" color="text.secondary" component="div">
+          { product.condition }
+        </Typography>
+        </CardContent>
+          <button>
+            BUY
+          </button>
+          <button className="back-btn" /* onClick={BackToAllProducts} */>
+            Back To All Products
+          </button>
         </Box>
-      </Card>
-    </ProductDetailsWrapper>
+    </Card>
+  </ProductDetailsWrapper>
   )
 }
 
